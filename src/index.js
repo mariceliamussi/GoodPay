13888999a6dea51c9a6674cf893e26e18dd800ac
Mari <maricelia.mussi@gmail.com>
2022-04-19 17:18:10 -0300

    Adição dos scripts de login e modal.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-function rotaCadastro(){
+var janelaLogin = document.getElementById('loginNegado')
 
+function rotaCadastro(){
+    window.location.href ='../registro.html';
 }
 function rotaRecuperar(){
-
+    window.location.href ='../recuperar.html';
 }
 function rotaLogin(){
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    if(email ==='victoricoma@gmail.com' && senha==='gordinho123'){
+        window.location.href='../index.html';
+    }else{
+        alert(`O login ${email} e a senha ${senha} não são viáveis`)
+    }
     
 }
