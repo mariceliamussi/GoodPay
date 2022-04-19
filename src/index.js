7b3eed0db40d4ff77ad2750885b794b52be4bebd
Mari <maricelia.mussi@gmail.com>
2022-04-19 18:38:13 -0300

    Adição dos elementos Javascript com Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-var janelaLogin = document.getElementById('loginNegado')
+var janelaLogin = new bootstrap.Modal(document.getElementById("loginNegado"));
 
-function rotaCadastro(){
-    window.location.href ='../registro.html';
+function rotaCadastro() {
+  window.location.href = "../registro.html";
 }
-function rotaRecuperar(){
-    window.location.href ='../recuperar.html';
+function rotaRecuperar() {
+  window.location.href = "../recuperar.html";
 }
-function rotaLogin(){
-    let email = document.getElementById('email').value;
-    let senha = document.getElementById('senha').value;
-    if(email ==='victoricoma@gmail.com' && senha==='gordinho123'){
-        window.location.href='../index.html';
-    }else{
-        alert(`O login ${email} e a senha ${senha} não são viáveis`)
-    }
-    
+function rotaLogin() {
+  let email = document.getElementById("email").value;
+  let senha = document.getElementById("senha").value;
+  if (email === "victoricoma@gmail.com" && senha === "gordinho123") {
+    window.location.href = "../index.html";
+  } else {
+    janelaLogin.show();
+  }
 }
