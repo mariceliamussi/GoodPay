3d3729df2f508d0a051d303a98463b2b6ee62d31
Mari <maricelia.mussi@gmail.com>
2022-04-20 10:40:52 -0300

    Modal controlado por instância em Javascript.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var janelaLogin = new bootstrap.Modal(document.getElementById("loginNegado"));
+var janelaPromocao = new bootstrap.Modal(document.getElementById("promo1"));
 
 function rotaCadastro() {
   window.location.href = "../registro.html";
@@ -14,4 +15,14 @@ function rotaLogin() {
   } else {
     janelaLogin.show();
   }
+}
+
+function alertaPromo(){
+  let titulo ="Promomção 1 [Quebra da Banca]";
+  let corpo = "Super legal <br> <img src='./img/4.png' width='160' height='160'>";
+  
+  janelaPromocao.show();
+
+  document.getElementById("titulo").innerHTML = titulo;
+  document.getElementById("corpo").innerHTML = corpo; 
 }
